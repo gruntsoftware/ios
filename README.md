343210ed890b3d0b9c650fd55a9f4dbbf260c23b
Hassan Ashraf <hassan.ashraf@transflo.com>
2025-03-10 16:53:46 +0200

    refactor: squash history to single commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# breadwallet-ios
---
+++ brainwallet/SeedWordView.swift
@@ -0,0 +1,61 @@
+import Foundation
+import SwiftUI
+
+struct SeedWord: Identifiable {
+	let id = UUID()
+	let word: String
+	let tagNumber: Int
+}
+
+struct SeedWordView: View {
+	let seedWord: String
+	let wordNumber: Int
+	let genericPad = 55.0
+	let cellHeight = 45.0
+	let offsetPad = 15.0
+	let topPad = -11.0
+
+	var body: some View {
+		GeometryReader { geometry in
+
+			let width = geometry.size.width
+			let height = geometry.size.height
+			ZStack {
+				VStack {
+					ZStack {
+						RoundedRectangle(cornerRadius: bigButtonCornerRadius)
+							.frame(height: cellHeight, alignment: .center)
+                            .foregroundColor(Color.brainwalletGray)
+							.shadow(radius: 3, x: 3.0, y: 3.0)
+
+						Text(seedWord)
+							.frame(height: cellHeight, alignment: .center)
+							.font(.barlowSemiBold(size: 16.0))
+							.foregroundColor(.black)
+
+						VStack {
+							HStack {
+								Text("\(wordNumber)")
+									.font(.barlowSemiBold(size: 14.0))
+									.foregroundColor(.brainwalletGray)
+									.frame(width: genericPad,
+									       height: cellHeight,
+									       alignment: .leading)
+									.offset(x: offsetPad,
+									        y: topPad)
+								Spacer()
+							}
+						}
+					}
+				}
+				.frame(width: width, height: height)
+				.padding(.all, genericPad)
+			}
+			.frame(width: width, height: height)
+		}
+	}
+}
+
+// #Preview {
+//	SeedWordView(seedWord: SeedWord(word: "banana"), wordNumber: 1)
+// }
