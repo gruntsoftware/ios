1ae396c108f914ebe93912ac25b0c011e6be29f3
Kerry Washington <mrkerrywashington@icloud.com>
2025-04-11 20:39:21 +0100

    Removed Today Extension
    - Removed nothing test

--- brainwalletTests/Constants Tests/ConstantsTests.swift
+++ brainwalletTests/Constants Tests/ConstantsTests.swift
@@ -2,7 +2,4 @@
 import XCTest
 
 class ConstantsTests: XCTestCase {
-	func testLFDonationAddressPage() throws {
-		XCTAssertTrue(FoundationSupport.dashboard == "https://support.brainwallet.io/")
-	}
 }
