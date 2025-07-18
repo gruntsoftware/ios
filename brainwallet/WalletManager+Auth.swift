1970de72ac61e845567711bf251d3a567c68cf7e
Kerry Washington <mrkerrywashington@icloud.com>
2025-07-18 15:11:24 +0100

    refactor crash in WalletManager+Auth
    using bundle exec fastlane single_unit_test_all
    downgrade firebase to 11.12.0
    polsih ci config

--- brainwallet/WalletManager+Auth.swift
+++ brainwallet/WalletManager+Auth.swift
@@ -40,11 +40,14 @@ extension WalletManager: WalletAuthenticator {
 	private static var failedPins = [String]()
 
 	convenience init(store: Store, dbPath: String? = nil) throws {
-		Task {
-			if await !UIApplication.shared.isProtectedDataAvailable {
-				throw NSError(domain: NSOSStatusErrorDomain, code: Int(errSecNotAvailable))
-			}
-		}
+
+        Task {
+            try await MainActor.run {
+                if !UIApplication.shared.isProtectedDataAvailable {
+                    throw NSError(domain: NSOSStatusErrorDomain, code: Int(errSecNotAvailable))
+                }
+            }
+        }
 
 		if try keychainItem(key: KeychainKey.seed) as Data? != nil { // upgrade from old keychain scheme
 			let seedPhrase: String? = try keychainItem(key: KeychainKey.mnemonic)
