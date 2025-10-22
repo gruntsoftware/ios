a44c92ab187c88a2b3852a8038f59e6b89b949c1
Kerry Washington <mrkerrywashington@icloud.com>
2025-10-22 12:27:20 +0100

    Completed added transaction details

--- brainwallet/Bento Views/TransactionDetailBentoView.swift
+++ brainwallet/Bento Views/TransactionDetailBentoView.swift
@@ -23,7 +23,7 @@ struct TransactionDetailBentoView: View {
     var copiedData: String = ""
 
     @State
-    var shouldShowSettings: Bool = false
+    var shouldShowExportProducts: Bool = false
 
     @Binding
     var userPrefersDarkTheme: Bool
@@ -52,101 +52,142 @@ struct TransactionDetailBentoView: View {
         GeometryReader { geometry in
 
             let width = geometry.size.width
+            let height = geometry.size.height
+
             ZStack {
                 BentoBackgroundView(userPrefersDarkTheme: $userPrefersDarkTheme)
                     .edgesIgnoringSafeArea(.all)
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            HStack {
 
-               VStack {
-
-                            // MARK: Amount data
-
-                            Group {
-                                VStack(alignment: .leading) {
-                                    HStack {
-                                        Text(cellViewModel.feeText)
-                                            .font(Font(UIFont.barlowRegular(size: 15.0)))
-                                            .lineLimit(3)
-                                            .scaledToFill()
-                                            .foregroundColor(BrainwalletColor.content)
-                                            .padding(.leading, 20.0)
-                                            .padding(.top, 10.0)
-
-                                        Spacer()
-                                    }
-                                    .padding(.top, 1.0)
-
-                                    Text(cellViewModel.addressText)
-                                        .font(Font(UIFont.barlowRegular(size: 15.0)))
-                                        .foregroundColor(BrainwalletColor.content)
-                                        .padding(.leading, 20.0)
-
-                                    Text(cellViewModel.transaction.hash)
-                                        .font(Font(UIFont.barlowLight(size: 9.0)))
-                                        .foregroundColor(BrainwalletColor.content)
-                                        .padding(.leading, 20.0)
-                                        .padding(.trailing, 40.0)
-
-                                    Text(String(localized: "Memo: ") + cellViewModel.memoString)
-                                        .font(Font(UIFont.barlowRegular(size: 15.0)))
-                                        .foregroundColor(BrainwalletColor.content)
-                                        .padding(.leading, 20.0)
-
-                                }
-                                .padding(.bottom, 2.0)
+                                Text(String(localized: "Amount detail: "))
+                                    .font(.system(size: 13, weight: .semibold, design: .default))
+                                    .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                                    .padding(.leading, 20.0)
+                                Text(String(cellViewModel.feeText))
+                                    .font(.system(size: 12, weight: .light, design: .default))
+                                    .lineLimit(4)
+                                    .minimumScaleFactor(0.8)
+                                    .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                    .padding(.trailing, 20.0)
 
                             }
-
-                            // MARK: Transaction data
-
-                            Group {
-
-                                VStack(alignment: .leading, spacing: 1.0) {
-
-                                    Text( String(localized: "Blockheight: ") + cellViewModel.transaction.blockHeight)
-                                        .font(Font(UIFont.barlowRegular(size: 15.0)))
-                                        .foregroundColor(BrainwalletColor.content)
-                                        .padding(.leading, 20.0)
-
-                                }
-                                .frame(height: 44)
+                            .padding(.top, 1.0)
+                            HStack {
+                                Text(String(localized: "Address: "))
+                                    .font(.system(size: 13, weight: .semibold, design: .default))
+                                    .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                    .padding(.leading, 20.0)
+                                Text(cellViewModel.addressText)
+                                    .font(.system(size: 12, weight: .light, design: .default))
+                                    .lineLimit(2)
+                                    .minimumScaleFactor(0.8)
+                                    .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                    .padding(.trailing, 20.0)
 
                             }
+                            .padding(.top, 1.0)
+                            HStack {
+                                Text(String(localized: "Transaction Hash: "))
+                                    .font(.system(size: 13, weight: .semibold, design: .default))
+                                    .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                    .padding(.leading, 20.0)
+                                Text(cellViewModel.transaction.hash)
+                                    .font(.system(size: 12, weight: .light, design: .default))
+                                    .lineLimit(3)
+                                    .minimumScaleFactor(0.8)
+                                    .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                    .padding(.trailing, 20.0)
+
+                            }
+                            .padding(.top, 1.0)
+                            HStack {
+                                Text(String(localized: "Blockheight: "))
+                                    .font(.system(size: 13, weight: .semibold, design: .default))
+                                    .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                    .padding(.leading, 20.0)
+                                Text(cellViewModel.transaction.blockHeight)
+                                    .font(.system(size: 12, weight: .light, design: .default))
+                                    .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                    .frame(alignment: .trailing)
+                                    .padding(.trailing, 20.0)
+
+                            }
+                            .padding(.top, 1.0)
+
+                            HStack {
+                                Text(String(localized: "Memo: "))
+                                    .font(.system(size: 13, weight: .semibold, design: .default))
+                                    .minimumScaleFactor(0.8)// Shrinks to 30% of original
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                    .padding(.leading, 20.0)
+
+                                Spacer()
+                            }
+                            .padding(.top, 1.0)
+                            HStack {
+                                Text(cellViewModel.memoString)
+                                    .font(.system(size: 12, weight: .light, design: .default))
+                                    .lineLimit(4)
+                                    .minimumScaleFactor(0.8)// Shrinks to 30% of original
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                    .padding(.trailing, 20.0)
+
+                                Spacer()
+                            }
+                            .padding(.top, 1.0)
 
                             // MARK: QR Image
-                            Group {
+                            HStack {
                                 Spacer()
-
-                                VStack(alignment: .center, spacing: 1.0) {
-                                    Image(uiImage: cellViewModel.qrImage)
-                                        .frame(width: kQRImageSide,
-                                               height: kQRImageSide,
-                                               alignment: .center)
-                                        .padding(.all, 2.0)
-                                        .tint(BrainwalletColor.content)
-
-                                    Text(cellViewModel.addressText)
-                                        .font(Font(UIFont.barlowLight(size: 13.0)))
-                                        .foregroundColor(BrainwalletColor.content)
-                                        .frame(alignment: .center).padding(.all, 2.0)
-                                }
-                                .padding(.all, 8.0)
-
+                                Image(uiImage: cellViewModel.qrImage)
+                                    .resizable()
+                                    .frame(width: kQRImageSide,
+                                       height: kQRImageSide,
+                                       alignment: .center)
+                                .padding(.all, 2.0)
+                                .tint(BrainwalletColor.content)
                                 Spacer()
                             }
 
-                            Spacer()
-                        }
-                VStack {
-                    Spacer()
-                    ExportButtonView(userPrefersDarkTheme: $userPrefersDarkTheme, viewModel: exportViewModel)
-                        .frame(width: width * 0.3, height: 35)
-                }
+                            }
 
+                    }
+                    .frame(width: width, height: height * 0.85)
+                    HStack {
+
+                        Button(action: {
+                            withAnimation(.spring(response: 0.4, dampingFraction: 0.8)) {
+                                shouldShowExportProducts.toggle()
+                            }
+                        }, label: {
+                            ZStack {
+                                BentoBackgroundView(userPrefersDarkTheme: $userPrefersDarkTheme)
+                                    .cornerRadius(bentoCornerRadius)
+                                    .modifier(BentoShadow(userPrefersDarkTheme: $userPrefersDarkTheme))
+
+                                VStack {
+                                    Text("Export Transaction Data")
+                                        .font(.system(size: 20, weight: .bold, design: .default))
+                                        .foregroundStyle( userPrefersDarkTheme ? .white.opacity(0.8): BrainwalletColor.nearBlack.opacity(0.8))
+                                }
+                            }
+                        })
+                        .frame(width: width * 0.8, height: 44)
+                        .padding(20.0)
+                    }
+                    .frame(width: width, height: height * 0.15)
                 }
-                .cornerRadius(bentoCornerRadius)
-                .onAppear {
-                    mainGradientStyle = userPrefersDarkTheme ? .darkStyle : .lightStyle
-                }
+            }
+            .cornerRadius(bentoCornerRadius)
+            .onAppear {
+                mainGradientStyle = userPrefersDarkTheme ? .darkStyle : .lightStyle
+            }
         }
     }
 }
