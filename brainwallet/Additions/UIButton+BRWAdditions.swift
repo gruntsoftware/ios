e0a45c749a87ca24410d4ccb35a1cef05aa1a998
Kerry Washington <mrkerrywashington@icloud.com>
2026-02-16 13:18:47 +0000

    Removed unused code

--- brainwallet/Additions/UIButton+BRWAdditions.swift
+++ brainwallet/Additions/UIButton+BRWAdditions.swift
@@ -19,14 +19,6 @@ extension UIButton {
 		return UIButton.icon(image: #imageLiteral(resourceName: "Close"), accessibilityLabel: accessibilityLabel)
 	}
 
-//	static func buildFaqButton(store: Store, articleId: String) -> UIButton {
-//		let button = UIButton.icon(image: #imageLiteral(resourceName: "Faq"), accessibilityLabel: "S.AccessibilityLabels.faq" )
-//		button.tap = {
-//			store.trigger(name: .presentFaq(articleId))
-//		}
-//		return button
-//	}
-
 	static func icon(image: UIImage, accessibilityLabel: String) -> UIButton {
 		let button = UIButton(type: .system)
 		button.frame = CGRect(x: 0, y: 0, width: 44, height: 44)
