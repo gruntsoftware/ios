c4f0ef55cfd38c8c6626756d3e5804399b862d26
Kerry Washington <mrkerrywashington@icloud.com>
2026-02-23 08:29:48 +0000

    Feat/add emoji picker (#92)
    * Improved with MVP
    
    WIP: Showing the old mvp picker
    
    WIP: added the picker
    
    Created the enum for the triplets
    
    WIP: Added more functionality
    
    WIP: Adding more elements in the picker
    
    Got the triplet working on a basic level
    
    WIP
    
    WIP: Working on the logic for the setp emojis
    
    WIP: Sorting out the logic
    
    WIP: Working in the EmojiText field
    
    WIP: Working 90%
    
    WIP: Addeing the settings for Brainwallet emojis
    
    WIP Building the Delete and set emojis
    
    WIP...need to sort out the WalletManager
    
    WIP ...making progress
    
    Passing the RC
    
    Passing the RC
    
    * Readded EmojiViewModel
    
    Added test point
    Added UnitTests

--- brainwallet/New Main Classes/NewMainViewModel.swift
+++ brainwallet/New Main Classes/NewMainViewModel.swift
@@ -188,6 +188,20 @@ class NewMainViewModel: ObservableObject, Subscriber {
 
     var resetSettingsDrawer: (() -> Void)?
 
+    @Published
+    var currentEmojiTriplet: EmojiTriplet = .first
+
+    @Published
+    var didSelectTriplet: Bool = false
+
+    @Published
+    var canSelect: Bool = false
+
+    @Published
+    var tripletDictionary: [Int : String] = [1 : "",
+                                            2 : "",
+                                            3  : ""]
+
     init(store: Store, walletManager: WalletManager) {
         self.store = store
         self.walletManager = walletManager
@@ -232,6 +246,14 @@ class NewMainViewModel: ObservableObject, Subscriber {
         NotificationCenter.default.removeObserver(self, name: .languageChangedNotification, object: nil)
         updateTimer?.invalidate()
         self.updateTimer = nil
+    }
+
+    func setEmojiTriplet() -> Bool {
+        guard let walletManager = self.walletManager else { return false }
+        let first = tripletDictionary[1] ?? ""
+        let second = tripletDictionary[2] ?? ""
+        let third = tripletDictionary[3] ?? ""
+        return walletManager.updateEmojiString("\(first)\(second)\(third)")
     }
 
     private func setBalances() {
