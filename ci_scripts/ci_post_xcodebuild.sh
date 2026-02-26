92e9b1841afbfdcf5d2e41420001b0b5f28b50b2
Kerry Washington <mrkerrywashington@icloud.com>
2026-02-26 12:58:30 +0000

    Added these other scripts for Xcode Cloud to silence warning

---
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# ci_scripts/ci_post_xcodebuild.sh
+# Requirement per the Xcode Cloud path
---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# ci_scripts/ci_pre_xcodebuild.sh
+# Requirement per the Xcode Cloud path
