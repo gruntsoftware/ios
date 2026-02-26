aa160d4dfb0bfc43912ae47d66f9e0895d639263
Kerry Washington <mrkerrywashington@icloud.com>
2026-02-26 12:28:23 +0000

    Added lines for the 2 other env files

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,5 +3,7 @@
 # Exit on error
 set -e
 
-# Write the plist to the correct location
-echo "$GOOGLE_SERVICE_INFO_PLIST" > "$CI_PRIMARY_REPOSITORY_PATH/brainwallet/PreLaunchResources/GoogleService-Info.plist"
+# Write the files to the correct location
+echo "GOOGLE_SERVICES_PLIST" > "$CI_PRIMARY_REPOSITORY_PATH/brainwallet/PreLaunchResources/GoogleService-Info.plist"
+echo "REMOTE_CONFIG_DEFAULTS" > "$CI_PRIMARY_REPOSITORY_PATH/brainwallet/PreLaunchResources/remote-config-defaults.plist"
+echo "DEBUG_SERVICE_DATA" > "$CI_PRIMARY_REPOSITORY_PATH/brainwallet/PreLaunchResources/service-data.plist"
